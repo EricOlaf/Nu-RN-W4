cf94d3f5035c4eea59c085ddf41e5fe9dd89706a
Minae Lee <minae.lee@gmail.com>
2020-11-30 00:59:40 -0800

    Initial commit

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
-import { ScrollView } from 'react-native';
+import { ScrollView, Text } from 'react-native';
+import { Card } from 'react-native-elements';
 
 class Contact extends Component {
 
-    static navigationOptions ={
+    static navigationOptions = {
         title: 'Contact Us'
-    };
+    }
 
     render() {
         return (
             <ScrollView>
-
+                <Card
+                    title="Contact Information"
+                    wrapperStyle={{margin: 20}}>
+                    <Text>1 Nucamp Way</Text>
+                    <Text>Seattle, WA 98001</Text>
+                    <Text style={{marginBottom: 10}}>U.S.A.</Text>
+                    <Text>Phone: 1-206-555-1234</Text>
+                    <Text>Email: campsites@nucamp.co</Text>
+                </Card>
             </ScrollView>
         );
     };
