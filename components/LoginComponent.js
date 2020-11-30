7d641ea316b01a21660182f1b2f75ad60082455b
Minae Lee <minae.lee@gmail.com>
2020-11-30 01:47:25 -0800

    Initial commit

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -42,8 +42,8 @@ class LoginTab extends Component {
                 })
             ).catch(error => console.log('Could not save user info', error));
         } else {
-            SecureStore.deleteItemAsync('userinfo').catch(error => 
-                console.log('Could not delete user info', error)
+            SecureStore.deleteItemAsync('userinfo').catch(
+                error => console.log('Could not delete user info', error)
             );
         }
     }
@@ -174,9 +174,24 @@ class RegisterTab extends Component {
             {format: 'png'}
         );
         console.log(processedImage);
-        this.setState({imageUrl: processedImage.uri });
+        this.setState({imageUrl: processedImage.uri});
     }
 
+    getImageFromGallery = async () => {
+        const cameraRollPermission = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+
+        if (cameraRollPermission.status === 'granted') {
+            const capturedImage = await ImagePicker.launchImageLibraryAsync({
+                allowsEditing: true,
+                aspect: [1, 1]
+            });
+            if (!capturedImage.cancelled) {
+                console.log(capturedImage);
+                this.processImage(capturedImage.uri);
+            }
+        }
+    }
+    
     handleRegister() {
         console.log(JSON.stringify(this.state));
         if (this.state.remember) {
@@ -203,6 +218,10 @@ class RegisterTab extends Component {
                         <Button
                             title='Camera'
                             onPress={this.getImageFromCamera}
+                        />
+                        <Button
+                            title='Gallery'
+                            onPress={this.getImageFromGallery}
                         />
                     </View>
                     <Input
