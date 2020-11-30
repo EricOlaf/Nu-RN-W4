9b18e601712a8da85464f841a18b0aba81aaddf7
Minae Lee <minae.lee@gmail.com>
2020-11-30 01:47:44 -0800

    Initial commit

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -330,16 +330,20 @@ class Main extends Component {
         this.props.fetchPromotions();
         this.props.fetchPartners();
 
-        NetInfo.fetch().then(connectionInfo => {
-            (Platform.OS === 'ios')
-                ? Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
-                : ToastAndroid.show('Initial Network Connectivity Type: ' +
-                    connectionInfo.type, ToastAndroid.LONG);
-        });
+        this.showNetInfo();
 
         this.unsubscribeNetInfo = NetInfo.addEventListener(connectionInfo => {
             this.handleConnectivityChange(connectionInfo);
         });
+    }
+
+    async showNetInfo() {
+        const connectionInfo = await NetInfo.fetch();
+
+            (Platform.OS === 'ios') 
+                ? Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
+                : ToastAndroid.show('Initial Network Connectivity Type: ' +
+                    connectionInfo.type, ToastAndroid.LONG);
     }
 
     componentWillUnmount() {
