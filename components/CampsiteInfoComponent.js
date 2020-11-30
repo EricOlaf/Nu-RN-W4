cd2386bffbf943966add04aeb7cf52babf5d1543
Minae Lee <minae.lee@gmail.com>
2020-11-30 01:36:55 -0800

    Initial commit

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -28,7 +28,8 @@ function RenderCampsite(props) {
     const view = React.createRef();
 
     const recognizeDrag = ({dx}) => (dx < -200) ? true : false;
-
+    const recognizeComment = ({dx}) => (dx > 200) ? true : false;
+    
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
         onPanResponderGrant: () => {
@@ -58,6 +59,8 @@ function RenderCampsite(props) {
                     ],
                     { cancelable: false }
                 );
+            } else if (recognizeComment(gestureState)) {
+                props.onShowModal();
             }
             return true;
         }
